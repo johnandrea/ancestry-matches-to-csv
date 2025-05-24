b5a6458b58ef4561344afe4bff15e2457acb5c5c
johnandrea <46268748+johnandrea@users.noreply.github.com>
2025-05-24 12:01:37 -0300

    Handle new page format from Ancestry

--- extract-matches-to-csv.py
+++ extract-matches-to-csv.py
@@ -11,7 +11,7 @@ Name each one as .txt and place all together with this program.
 
 Options:
 --out-file default matches.csv
---skip-id default true
+--skip-id default false
 --id-with-name default false
 --skip-header default false
 --min-cm default 22
@@ -21,8 +21,8 @@ Needs: Python 3.6+
 
 This code is released under the MIT License:
 https://opensource.org/licenses/MIT
-Copyright (c) 2022 John A. Andrea
-v2.0
+Copyright (c) 2025 John A. Andrea
+v3.0
 
 No support provided.
 '''
@@ -74,44 +74,36 @@ def get_program_options():
 
     return results
 
-def get_owner_name( s, pattern1, pattern2 ):
-    result = None
-    m = pattern1.match( s )
-    if m:
-       result = m.group(1)
-    else:
-       m = pattern2.match( s )
-       if m:
-          result = m.group(1)
-    return result
-
-def get_url( s, line_pattern ):
-    ids = []
-    m = line_pattern.match( s )
-    if m:
-       ids.append( m.group(1) )
-       ids.append( m.group(2) )
-    return ids
-
-def get_cm( s, line_pattern ):
-    result = None
-    m = line_pattern.match( s.lower() )
-    if m:
-       # also remove the hundreds separator (,)
-       result = m.group(1).replace( ',', '' )
-    return result
 
 def escape_quote( s ):
     return str(s).replace( '"', '\"').replace( "'", "\'" )
 
-def output( one, two, three, four, five, f ):
+
+def quoted( s ):
+    return '"' + s.strip() + '"'
+
+
+def output( owner, match, owner_id, match_id, cm, f ):
     # as csv
-    out = '"' + one + '"'
-    out += ',"' + two + '"'
-    out += ',"' + three + '"'
-    if four:
-       out += ',"' + four + '","' + five + '"'
-    print( out, file=f )
+    if is_int( cm ) and int(cm) >= options['min-cm']:
+       name1 = escape_quote( owner )
+       name2 = escape_quote( match )
+
+       rest = ''
+       if options['add-id']:
+          if options['id-with-name']:
+             name1 += '/' + owner_id
+             name2 += '/' + match_id
+          else:
+             rest = ',' + quoted( owner_id )
+             rest += ',' + quoted( match_id )
+
+       out = quoted( name1 )
+       out += ',' + quoted( name2 )
+       out += ',' + quoted( cm )
+       out += rest
+       print( out, file=f )
+
 
 def is_int( s ):
     try:
@@ -123,15 +115,8 @@ def is_int( s ):
 
 options = get_program_options()
 
-name_pattern1 = re.compile( r'^(.*)\'s DNA Matches$' ) # account owner
-name_pattern2 = re.compile( r'^You and (.*)$' ) # matches of account owner
-url_pattern = re.compile( r'^.*discoveryui-matches/compare/([A-Za-z0-9-]*)/with/([A-Za-z0-9-]*)>' )
-cm_pattern = re.compile( r'^(.*) cm | ' )
-
-not_names = [] # must be lower case
-not_names.append( 'view match' )
-not_names.append( 'common ancestor' )
-not_names.append( 'do you recognize them?' )
+compareurl_pattern = re.compile( r'^.*discoveryui-matches/compare/([A-Za-z0-9-]*)/with/([A-Za-z0-9-]*)>' )
+cm_pattern = re.compile( r'^(.*) cM | ' )
 
 with open( options['out-file'], 'w' ) as outf:
 
@@ -141,61 +126,31 @@ with open( options['out-file'], 'w' ) as outf:
            header += ',"id 1","id 2"'
         print( header, file=outf )
 
-     # these are the case labels for the loop over lines
-     first_step = 'owner'
-     person_url = 'person url'
-     person_name = 'person name'
-     cm_tag = 'cm'
+     # as of 2025 the owner's name does not appear on the saved match page
+     owner_name = 'you'
 
      for filename in glob.glob( '*.txt' ):
          #print( filename, file=sys.stderr )
 
-         owner_name = None
-         match_name = None
-         url_ids = []
-         look_for = first_step
-
          with open( filename ) as inf:
+              match_name = ''
+              owner_id = ''
+              match_id = ''
+              prev_line = ''
               for line in inf:
                   line = line.strip()
                   if line:
-                     if look_for == first_step:
-                        name = get_owner_name( line, name_pattern1, name_pattern2 )
-                        if name:
-                           owner_name = escape_quote( name )
-                           look_for = person_url
+                     m = compareurl_pattern.match( line )
+                     if m:
+                        # name was previous line
+                        match_name = prev_line
+                        owner_id = m.group(1)
+                        match_id = m.group(2)
+                     else:
+                        m = cm_pattern.match( line )
+                        if m:
+                           # also remove the hundreds separator (,)
+                           cm = m.group(1).replace( ',', '' )
 
-                     elif look_for == person_url:
-                        url_ids = get_url( line, url_pattern )
-                        if url_ids:
-                           look_for = person_name
-
-                     elif look_for == person_name:
-                        # this will be the next line after the url
-                        if line.lower() in not_names:
-                           # this is a clickable item, keep looking for the person start line
-                           look_for = person_url
-                        else:
-                           match_name = line
-                           look_for = cm_tag
-
-                     elif look_for == cm_tag:
-                        cm = get_cm( line, cm_pattern )
-                        if cm:
-                           if is_int( cm ) and int(cm) >= options['min-cm']:
-                              name1 = owner_name
-                              name2 = escape_quote(match_name)
-                              if options['add-id']:
-                                 if options['id-with-name']:
-                                    output( name1 + '/' + url_ids[0], name2 + '/' + url_ids[1], str(cm), None, None, outf )
-                                 else:
-                                    output( name1, name2, str(cm), url_ids[0], url_ids[1], outf )
-                              else:
-                                 output( name1, name2, str(cm), None, None, outf )
-                           look_for = person_url
-
-              if not owner_name:
-                 print( filename, ':reached and of file without finding owner', file=sys.stderr )
-
-              if look_for != person_url:
-                 print( filename, 'didnt complete search', look_for, file=sys.stderr )
+                           output( owner_name, match_name, owner_id, match_id, cm, outf )
+                     prev_line = line
